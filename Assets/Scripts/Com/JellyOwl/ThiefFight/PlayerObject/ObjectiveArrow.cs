dca1dfeddfaa46a4805dfbadd39862cae5ff3d8f
Teo Diaz <diaz.teo@outlook.com>
2019-10-10 23:48:34 +0200

    add objective arrow branch

---
+++ Assets/Scripts/Com/JellyOwl/ThiefFight/PlayerObject/ObjectiveArrow.cs
@@ -0,0 +1,19 @@
+///-----------------------------------------------------------------
+/// Author : Teo Diaz
+/// Date : 10/10/2019 23:47
+///-----------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Com.JellyOwl.ThiefFight.PlayerObject {
+	public class ObjectiveArrow : MonoBehaviour {
+	
+		private void Start () {
+			
+		}
+		
+		private void Update () {
+			
+		}
+	}
+}
