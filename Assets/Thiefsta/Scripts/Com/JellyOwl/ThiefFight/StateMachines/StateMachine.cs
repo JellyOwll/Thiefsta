2bee910f51d8dd42bedc6787e634cc1b093b718d
Teo Diaz <diaz.teo@outlook.com>
2019-10-13 12:35:13 +0200

    add Normal State Machine

---
+++ Assets/Thiefsta/Scripts/Com/JellyOwl/ThiefFight/StateMachines/StateMachine.cs
@@ -0,0 +1,27 @@
+﻿///-----------------------------------------------------------------
+/// Author : Teo Diaz
+/// Date : 13/10/2019
+///-----------------------------------------------------------------
+
+using System;
+using UnityEngine;
+
+namespace Com.JellyOwl.ThiefFight.StateMachine {
+    public abstract class StateMachine : MonoBehaviour
+    {
+
+        protected Action DoAction;
+        protected abstract void DoActionVoid();
+        protected abstract void DoActionNormal();
+
+        protected virtual void Start()
+        {
+            SetModeVoid();
+        }
+
+        protected virtual void Update() => DoAction();
+
+        protected void SetModeNormal() => DoAction = DoActionNormal;
+        protected void SetModeVoid() => DoAction = DoActionVoid;
+    }
+}
